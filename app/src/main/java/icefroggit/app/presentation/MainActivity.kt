b676ecb432d1c357b97a63866bf5998856fa59bb
IceFroggit <zeksengali0@gmail.com>
2023-09-13 19:17:53 +0300

    Initial commit

---
+++ app/src/main/java/icefroggit/app/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package icefroggit.app.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import icefroggit.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
