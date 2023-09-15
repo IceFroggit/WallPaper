45aafc02672971559b080cde9bd657f4cd129ff1
IceFroggit <zeksengali0@gmail.com>
2023-09-15 14:06:55 +0300

    added category fragment and category recView

--- app/src/main/java/icefroggit/app/presentation/fragments/CategoriesFragment.kt
+++ app/src/main/java/icefroggit/app/presentation/fragments/CategoriesFragment.kt
@@ -1,29 +1,34 @@
 package icefroggit.app.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import icefroggit.app.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import icefroggit.app.databinding.FragmentCategoriesBinding
-import icefroggit.app.presentation.adapter.RecyclerViewAdapter
+import icefroggit.app.presentation.adapter.CategoryAdapterRV
+import icefroggit.app.utils.ApiCategory
 
 
-class CategoriesFragment : BaseFragment<FragmentCategoriesBinding>(
-    FragmentCategoriesBinding::inflate
-) {
-    override var recyclerViewAdapter: RecyclerViewAdapter
-        get() = TODO("Not yet implemented")
-        set(value) {}
-
-    override fun initViewModel() {
-        TODO("Not yet implemented")
+class CategoriesFragment : Fragment() {
+    private lateinit var binding: FragmentCategoriesBinding
+    private lateinit var recyclerViewAdapter: CategoryAdapterRV
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentCategoriesBinding.inflate(inflater, container, false)
+        initRecyclerViewAdapter()
+        return binding.root
     }
+    private fun initRecyclerViewAdapter(){
+        val layoutManager = GridLayoutManager(context,3)
+        recyclerViewAdapter = CategoryAdapterRV(ApiCategory.list)
+        binding.categoriesRecyclerView.layoutManager = layoutManager
+        binding.categoriesRecyclerView.adapter = recyclerViewAdapter
 
-    override fun initRecyclerView() {
-        TODO("Not yet implemented")
     }
-
 
 }
